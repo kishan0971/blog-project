b084e3d58d7eba42c34e2171af47016b1b170d44
Kishan Kashyap <kishankashyap0971@gmail.com>
2024-07-31 15:04:29 +0530

    [UPD] updated in service

--- blog-service/src/main/java/com/in2it/blogservice/service/BlogService.java
+++ blog-service/src/main/java/com/in2it/blogservice/service/BlogService.java
@@ -1,5 +1,18 @@
 package com.in2it.blogservice.service;
 
+import java.util.List;
+
+import com.in2it.blogservice.dto.BlogDto;
+
 public interface BlogService {
+	
+	
+	BlogDto saveBlog(BlogDto blogDto);
+	BlogDto getByid(int id);
+	BlogDto updateBlog(BlogDto blogDto, int id);
+	BlogDto getBlogByTitle(String title);
+	List<BlogDto> getAllBlog();
+	void deleteBlog(int id);
+	
 
 }
