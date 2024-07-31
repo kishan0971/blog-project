8c97c570308db9ccb619f4fa3ed6a14612c40f2d
Vaseem Akhtar <vaseem.akhtar@in2ittech.com>
2024-07-31 16:02:46 +0530

    [ADD] swager

--- blog-service/src/main/java/com/in2it/blogservice/service/impl/BlogServiceImpl.java
+++ blog-service/src/main/java/com/in2it/blogservice/service/impl/BlogServiceImpl.java
@@ -1,6 +1,53 @@
 package com.in2it.blogservice.service.impl;
 
-public class BlogServiceImpl {
+import java.util.List;
 
-//	public void save
+import com.in2it.blogservice.dto.BlogDto;
+import com.in2it.blogservice.service.BlogService;
+
+public class BlogServiceImpl implements BlogService {
+
+	@Override
+	public BlogDto saveBlog(BlogDto blogDto) {
+		
+		return null;
+	}
+
+	@Override
+	public BlogDto updateBlog(BlogDto blogDto, Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteBlog(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BlogDto getByid(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BlogDto getBlogByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BlogDto> getAllBlog() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BlogDto> getByUserName(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
