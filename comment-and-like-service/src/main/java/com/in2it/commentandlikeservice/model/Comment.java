4881f888014abc549a035e20c066b1a296a238fe
Kishan Kashyap <kishankashyap0971@gmail.com>
2024-07-31 16:33:20 +0530

    [UPD] updated coment and like

--- comment-and-like-service/src/main/java/com/in2it/commentandlikeservice/model/Comment.java
+++ comment-and-like-service/src/main/java/com/in2it/commentandlikeservice/model/Comment.java
@@ -1,5 +1,19 @@
 package com.in2it.commentandlikeservice.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Comment {
+	
+	private long id;
+	private String content;
+	
+	private String media;
 
 }
