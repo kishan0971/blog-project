5b16121860b73dd2e252359e9bdb76abbb3df453
Kishan Kashyap <kishankashyap0971@gmail.com>
2024-07-31 11:52:48 +0530

    [UPD] updated model class

--- blog-service/src/main/java/com/in2it/blogservice/model/Blog.java
+++ blog-service/src/main/java/com/in2it/blogservice/model/Blog.java
@@ -1,7 +1,20 @@
 package com.in2it.blogservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class Blog {
 	
+	@Id
 	private int id;
 	private String title;
 	private String content;
