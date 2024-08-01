477f390d4fad372f9e202712a78b5160cd98e598
sidsh67 <sidsh67@gmail.com>
2024-08-01 14:34:30 +0530

    not commit

--- blog-service/src/main/java/com/in2it/blogservice/dto/BlogDto.java
+++ blog-service/src/main/java/com/in2it/blogservice/dto/BlogDto.java
@@ -3,10 +3,7 @@ package com.in2it.blogservice.dto;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,8 +18,11 @@ public class BlogDto {
 	
 	
 	private long id;
+	
 	private long departmentId;
+	
 	private long projectId;
+	
 	@Size(max = 50,min = 2,message = "title must not be longer than 50 characters and not be less than 3 characters")
 	@NotBlank(message = "title cannot be blank") 
 	private String title;
--- blog-service/src/main/java/com/in2it/blogservice/model/Blog.java
+++ blog-service/src/main/java/com/in2it/blogservice/model/Blog.java
@@ -2,16 +2,11 @@ package com.in2it.blogservice.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-<<<<<<< HEAD
-import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
-=======
->>>>>>> 4eb7335e8605256c91648e3be1d3602a7172f93d
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Blog {
 	
 	@Id
-	@GeneratedValue(strategy =  GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	private long departmentId;
 	private long projectId;
@@ -38,12 +33,12 @@ public class Blog {
 	private int commentCount;
 	private int likeCount;
 	private String media;
-<<<<<<< HEAD
+
 	@NotNull
 	private String postedBy;
-=======
+
 	private long authorId;
->>>>>>> 4eb7335e8605256c91648e3be1d3602a7172f93d
+
 	private LocalDateTime cretedDateTime;
 
 	
